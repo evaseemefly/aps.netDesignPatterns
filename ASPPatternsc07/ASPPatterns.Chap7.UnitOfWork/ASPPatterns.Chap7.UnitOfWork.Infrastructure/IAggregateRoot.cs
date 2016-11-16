94316ec1078b04ef9bbdca6e25341b658d5ec792
t430 <evaseemefly@126.com>
2016-11-16 21:08:53 +0800

    添加第七章

---
+++ ASPPatternsc07/ASPPatterns.Chap7.UnitOfWork/ASPPatterns.Chap7.UnitOfWork.Infrastructure/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASPPatterns.Chap7.UnitOfWork.Infrastructure
+{
+    /// <summary>
+    /// 聚合根
+    /// </summary>
+    public interface IAggregateRoot
+    {
+    }
+}
