4aac00bf78772e332ad33fb503e900afe69de320
t430 <evaseemefly@126.com>
2016-12-14 21:06:36 +0800

    12月14日，家中学习

--- ASPPatternsc07/ASPPatterns.Chap7.Library/ASPPatterns.Chap7.Library.Repository.EF/DataContextFactory.cs
+++ ASPPatternsc07/ASPPatterns.Chap7.Library/ASPPatterns.Chap7.Library.Repository.EF/DataContextFactory.cs
@@ -11,8 +11,10 @@ namespace ASPPatterns.Chap7.Library.Repository.EF
     {      
         public static LibraryDataContext GetDataContext()
         {
-            IDataContextStorageContainer _dataContextStorageContainer = DataContextStorageFactory.CreateStorageContainer(); 
+            //由工厂方法获取贮藏存储对象
+            IDataContextStorageContainer _dataContextStorageContainer = DataContextStorageFactory.CreateStorageContainer();
 
+            //由于贮藏存储对象实现了指定接口，即实现了GetDataContext方法，调用该方法获取上下文对象
             LibraryDataContext libraryDataContext = _dataContextStorageContainer.GetDataContext();
             if (libraryDataContext == null)
             {
--- ASPPatternsc07/ASPPatterns.Chap7.Library/ASPPatterns.Chap7.Library.Repository.EF/DataContextStorage/DataContextStorageFactory.cs
+++ ASPPatternsc07/ASPPatterns.Chap7.Library/ASPPatterns.Chap7.Library.Repository.EF/DataContextStorage/DataContextStorageFactory.cs
@@ -6,6 +6,9 @@ using System.Web;
 
 namespace ASPPatterns.Chap7.Library.Repository.EF.DataContextStorage
 {
+    /// <summary>
+    /// 数据上下文贮藏工厂，由外侧的数据上下文工厂调用
+    /// </summary>
     public class DataContextStorageFactory
     {
         public static IDataContextStorageContainer _dataContectStorageContainer;
